9d57896c1a3e4da4ac5bff902cf24fae57031cb3
svkashini <koki.kashihara@gmail.com>
2021-07-22 15:43:10 +0900

    Krispの結果の閲覧機能を追加

--- motogymkhana_analysis.py
+++ motogymkhana_analysis.py
@@ -5,18 +5,70 @@ import pandas as pd
 import streamlit as st
 from streamlit.logger import DEFAULT_LOG_MESSAGE
 
-df = pd.read_csv('data.csv', index_col=0)
-df = df.replace({'NN':'N', 'NO':'N', 'NX':'N'})
-df['Season'] = df['Date'].apply(lambda x: int(x[:4]))
-
-color_class = {'A':'red', 'B':'blue', 'C1':'lightgreen', 'C2':'limegreen', 'N':'yellow', 'NL':'pink'}
-color_man = {'Honda':'red', 'Suzuki':'yellow', 'Kawasaki':'limegreen', 'Yamaha':'blue', 'KTM':'orange', 'Ducati':'crimson'}
-list_man = df['Manufacturer'].unique()
-
 st.title('MotoGymkhana Result Database')
 st.text('出典: 二輪ジムカーナ主催者団体協議会（ＪＡＧＥ） http://jage.jpn.org/claro/index.html')
-st.text('データはJAGE HP記載の2011年以降のDUNLOP杯、JAGE杯のリザルトを加工し作成')
+st.text('出典: KRiSP(二輪の安全運転練習会・オンロード) | キョウセイドライバーランド https://www.kotsu-daigaku.jp/krisp.html')
+st.text('データは出典元HP記載のリザルトを加工し作成')
 
+add_selectbox = st.sidebar.selectbox(
+    '参照データを選択',
+    ('関東: DUNROP杯, JAGE杯 (2011〜)', '中部: KRiSP（2017〜）')
+)
+
+add_checkbox = st.sidebar.checkbox(
+    'シーズン中の同一ライダーの重複を除く'
+)
+
+if add_selectbox == '関東: DUNROP杯, JAGE杯 (2011〜)':
+    #関東データに適用
+    df = pd.read_csv('data.csv', index_col=0)
+    df = df.replace({'NN':'N', 'NO':'N', 'NX':'N'})
+    df['Season'] = df['Date'].apply(lambda x: int(x[:4]))
+    df = pd.read_csv('data.csv', index_col=0)
+    df = df.replace({'NN':'N', 'NO':'N', 'NX':'N'})
+    df['Season'] = df['Date'].apply(lambda x: int(x[:4]))
+else:
+    df = pd.read_csv('data_krisp.csv', index_col=0)
+
+
+add_slider = st.sidebar.slider(
+    'シーズンを選択',
+    value = df['Season'].max().item(),
+    min_value = df['Season'].min().item(),
+    max_value = df['Season'].max().item(),
+    step = 1
+)
+
+color_class = {
+    'A':'red',
+    'B':'blue',
+    'C1':'lightgreen',
+    'C2':'limegreen',
+    'N':'yellow',
+    'NL':'pink',
+    'C0':'green',
+    'D1':'gray',
+    'D2':'yellow',
+    'D3':'orange'
+    }
+
+color_man = {
+    'Honda':'red',
+    'Suzuki':'yellow',
+    'Kawasaki':'limegreen',
+    'Yamaha':'blue',
+    'KTM':'orange',
+    'Ducati':'crimson'
+    }
+
+list_man = df['Manufacturer'].unique()
+
+m_list2 = ['全メーカー'] + list_man.tolist()
+
+option_man = st.sidebar.selectbox(
+    'メーカーを選択',
+    m_list2,
+)
 
 st.title('【シーズン別出走車両台数推移】')
 st.text('注記）のべエントリー数で集計')
@@ -39,9 +91,8 @@ df_season
 
 
 st.title('シーズン・車両別出走台数')
-ex_dup = st.checkbox('シーズン中の同一ライダーの重複を除く:')
 
-if ex_dup == True:
+if add_checkbox == True:
     temp = pd.DataFrame()
     for year in df['Season'].unique():
         df_year = df[df['Season']==year].sort_values('Class')
@@ -53,93 +104,41 @@ else:
 
 df_m = df_m.groupby(['Season', 'Machine', 'Manufacturer'], as_index=False).count()
 
-option_season = st.slider(
-    'シーズンを選択',
-    value = 2021,
-    min_value = 2011,
-    max_value = 2021,
-    step = 1
-)
-    
-m_list2 = ['全メーカー'] + list_man.tolist()
-#m_list2.append((np.sort(list_man).tolist()))
+if option_man != '全メーカー':
+    df_m = df_m[df_m['Manufacturer']==option_man]
 
-option_man2 = st.selectbox(
-    '抽出対象メーカーを選択',
-    m_list2
-)
+df_m = df_m[df_m['Season'] == add_slider]
 
-if option_man2 != '全メーカー':
-    df_m = df_m[df_m['Manufacturer']==option_man2]
-
-df_m = df_m[df_m['Season'] == option_season]
-
-
+disp_max = len(df_m['Machine'].unique())
+disp_min = disp_max - 15.5
 
 fig = px.bar(
     data_frame = df_m.sort_values(['Rider']),
     x = 'Rider',
     y = 'Machine',
+    range_y = [disp_min, disp_max],
+    title = str(add_slider) + '年の出走台数上位15車種 (' + str(option_man) + ')'
 )
 
 st.plotly_chart(fig)
 
 
+#車両別データの表示
+st.title('【車両別データ】')
 
-st.title('クラス昇格者の使用車両')
-classup_list = ['Bへ昇格', 'C1へ昇格', 'C2へ昇格']
-option_classup = st.selectbox('選択して下さい', classup_list)
-
-#B級昇格者の抽出
-to_b = df[df['Rating[%]'] < 105]
-to_b = to_b[(to_b['Class'] !='A') & (to_b['Class']!= 'B')]
-
-#C1級昇格者の抽出
-to_c1 = df[df['Rating[%]'] < 110]
-to_c1 = to_c1[(to_c1['Class'] !='A') & (to_c1['Class']!= 'B') & (to_c1['Class']!= 'C1')]
-
-#C2級昇格者の抽出
-to_c2 = df[df['Rating[%]'] < 115]
-to_c2 = to_c2[to_c2['Class'].str.contains('N')]
-
-if option_classup == 'Bへ昇格':
-    df_classup = to_b
-elif option_classup == 'C1へ昇格':
-    df_classup = to_c1
+if option_man == '全メーカー':
+    machine_list = ['全車種']
+    df_machine = df
 else:
-    df_classup = to_c2
-
-slider_range = st.slider(
-    '抽出期間を選択',
-    value = [2011, 2021],
-    min_value = 2011,
-    max_value = 2021,
-    step = 1
+    machine_list = df[df['Manufacturer']==option_man]['Machine'].unique()
+    option_machine = st.sidebar.selectbox(
+        '車両を選択',
+        np.sort(machine_list),
     )
 
-df_classup = df_classup[(df_classup['Season']>=slider_range[0])&(df_classup['Season']<=slider_range[1])]
-temp = df_classup.groupby(['Machine', 'Manufacturer'], as_index=False).count()[['Machine','Rider', 'Manufacturer']].sort_values('Rider', ascending=False)
-df_classup = df_classup['Machine'].value_counts()
+    df_machine = df[df['Machine']==option_machine]
 
-fig = px.bar(
-    df_classup.sort_values().reset_index().rename(columns={'index': 'Machine', 'Machine':'昇格者数（人）'}),
-    x = '昇格者数（人）',
-    y = 'Machine',
-    orientation = 'h'
-)
-st.plotly_chart(fig)
-temp
-
-
-
-st.title('【車両別データ】')
-option_man = st.selectbox('メーカーを選択', np.sort(list_man), index=4)
-machine_list = df[df['Manufacturer']==option_man]['Machine'].unique()
-option_machine = st.selectbox('車両を選択', np.sort(machine_list), index=10)
-df_machine = df[df['Machine']==option_machine]
-ex_dup = st.checkbox('シーズン中の同一ライダーの重複を除く')
-
-if ex_dup == True:
+if add_checkbox == True:
     temp = pd.DataFrame()
     for year in df_machine['Season'].unique():
         df_year = df_machine[df_machine['Season']==year].sort_values('Class')
@@ -149,10 +148,14 @@ if ex_dup == True:
 
 #df_machine
 
-st.header('出走台数推移')
-
+#出走台数推移
 year_max = df['Season'].max() + 1
 year_min = df['Season'].min() - 1
+
+if option_man == '全メーカー':
+    graph_title =  add_selectbox + ': 出走台数推移(' + str(option_man) + ')'
+else:
+    graph_title =  add_selectbox + ': 出走台数推移(' + str(option_man) +', ' + str(option_machine) + ')'
 
 fig = px.bar(
     data_frame = df_machine.groupby(['Season', 'Class'], as_index=False).count()[['Season', 'Class', 'Rider']].sort_values('Class'),
@@ -162,10 +165,9 @@ fig = px.bar(
     color = 'Class',
     color_discrete_map = color_class,
     labels = {'Rider':'出走台数（台）'},
+    title = graph_title
 )
 st.plotly_chart(fig)
-
-
 
 st.header('タイム比推移・分布')
 class_list = ['全て', 'A', 'B', 'C1', 'C2', 'N', 'NL']
@@ -174,7 +176,9 @@ if option_class == '全て':
     df_class = df
 else:
     df_class = df[df['Class']==option_class]
-df_class = df_class[df_class['Machine']==option_machine]
+
+if option_man != '全メーカー':
+    df_class = df_class[df_class['Machine']==option_machine]
 
 fig = px.scatter(
     data_frame = df_class.sort_values('Class'),
@@ -182,35 +186,17 @@ fig = px.scatter(
     y = 'Rating[%]',
     color = 'Class',
     color_discrete_map = color_class,
-    range_x = [datetime.datetime(2011,1,1), datetime.datetime.now()],
-    range_y = [df_class['Rating[%]'].max(), 100],
+    range_x = [datetime.datetime(year_min.item(),1,1), datetime.datetime(year_max.item(),1,1)],
+    range_y = [160, 100],
     labels = {'Rating[%]':'タイム比[%]'}
 )
 st.plotly_chart(fig)
 
-# df_class_min = pd.DataFrame(df_class.groupby('Date')['Rating[%]'].min()).rename(columns={'Rating[%]':'Top'})
-# df_class_ave = pd.DataFrame(df_class.groupby('Date')['Rating[%]'].mean()).rename(columns={'Rating[%]':'Average'})
-# df_class_max = pd.DataFrame(df_class.groupby('Date')['Rating[%]'].max()).rename(columns={'Rating[%]':'Worst'})
-# df_class = pd.concat([df_class_min, df_class_ave, df_class_max], axis=1)
-
-# #df_class
-# df_class = df_class.reset_index()
-
-# fig = px.line(
-#     data_frame = df_class,
-#     x = 'Date',
-#     y = ['Top', 'Average', 'Worst'],
-#     range_x = [datetime.datetime(2011,1,1), datetime.datetime.now()],
-#     range_y = [df_class['Worst'].max(), 100],
-#     labels = {'value':'タイム比[%]'}
-# )
-# st.plotly_chart(fig)
-
 slider_range2 = st.slider(
     '期間を選択',
-    value = [2011, 2021],
-    min_value = 2011,
-    max_value = 2021,
+    value = [year_min.item(), year_max.item()],
+    min_value = year_min.item(),
+    max_value = year_max.item(),
     step = 1
     )
 
@@ -224,3 +210,50 @@ fig = px.histogram(
 )
 
 st.plotly_chart(fig)
+
+
+
+if add_selectbox == '関東: DUNROP杯, JAGE杯 (2011〜)':
+    st.title('クラス昇格者の使用車両')
+    classup_list = ['Bへ昇格', 'C1へ昇格', 'C2へ昇格']
+    option_classup = st.selectbox('選択して下さい', classup_list)
+
+    #B級昇格者の抽出
+    to_b = df[df['Rating[%]'] < 105]
+    to_b = to_b[(to_b['Class'] !='A') & (to_b['Class']!= 'B')]
+
+    #C1級昇格者の抽出
+    to_c1 = df[df['Rating[%]'] < 110]
+    to_c1 = to_c1[(to_c1['Class'] !='A') & (to_c1['Class']!= 'B') & (to_c1['Class']!= 'C1')]
+
+    #C2級昇格者の抽出
+    to_c2 = df[df['Rating[%]'] < 115]
+    to_c2 = to_c2[to_c2['Class'].str.contains('N')]
+
+    if option_classup == 'Bへ昇格':
+        df_classup = to_b
+    elif option_classup == 'C1へ昇格':
+        df_classup = to_c1
+    else:
+        df_classup = to_c2
+
+    slider_range = st.slider(
+        '抽出期間を選択',
+        value = [2011, 2021],
+        min_value = 2011,
+        max_value = 2021,
+        step = 1
+        )
+
+    df_classup = df_classup[(df_classup['Season']>=slider_range[0])&(df_classup['Season']<=slider_range[1])]
+    temp = df_classup.groupby(['Machine', 'Manufacturer'], as_index=False).count()[['Machine','Rider', 'Manufacturer']].sort_values('Rider', ascending=False)
+    df_classup = df_classup['Machine'].value_counts()
+
+    fig = px.bar(
+        df_classup.sort_values().reset_index().rename(columns={'index': 'Machine', 'Machine':'昇格者数（人）'}),
+        x = '昇格者数（人）',
+        y = 'Machine',
+        orientation = 'h'
+    )
+    st.plotly_chart(fig)
+    temp
